a50ba8a02fd0b144d7c3d8a41c3809001d796fcd
ekkalak2424 <ai.ekkalak24@gmail.com>
2025-07-19 12:11:02 +0700

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,8 @@
+// Debug: ดู capabilities ของ user ปัจจุบัน
+add_action('admin_notices', function() {
+    $user = wp_get_current_user();
+    echo '<div class="notice notice-info"><p>Current user roles: ' . implode(', ', $user->roles) . '</p></div>';
+    echo '<div class="notice notice-info"><p>Capabilities: ' . implode(', ', array_keys($user->allcaps)) . '</p></div>';
+});
+
+
